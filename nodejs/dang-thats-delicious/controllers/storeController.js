0800bf7e5ae5380ac19bfe6019a864a19d095615
lgcaobianco <gu.caobianco@gmail.com>
2019-05-28 15:02:07 -0300

    End of lesson 17

--- nodejs/dang-thats-delicious/controllers/storeController.js
+++ nodejs/dang-thats-delicious/controllers/storeController.js
@@ -1,53 +1,52 @@
 const mongoose = require("mongoose");
-
 const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-  res.render("index", {
-    title: "Welcome!"
-  });
+  res.render("index");
 };
 
 exports.addStore = (req, res) => {
-  res.render("editStore", {
-    title: "Add Store"
-  });
+  res.render("editStore", { title: "Add Store" });
 };
 
 exports.createStore = async (req, res) => {
   const store = await new Store(req.body).save();
   req.flash(
     "success",
-    `Successfully created ${store.name}. Care to leave a review?`
+    `Successfully Created ${store.name}. Care to leave a review?`
   );
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
   const stores = await Store.find();
-  res.render("stores", {
-    title: "Stores",
-    stores: stores
-  });
+  res.render("stores", { title: "Stores", stores });
 };
 
 exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
-  console.log(store);
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
   res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = "Point";
+  // find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
-    new: true, // returns new store
-    runValidators: true // force the data to run through validators
+    new: true, // return the new store instead of the old one
+    runValidators: true
   }).exec();
-
   req.flash(
     "success",
-    `Successfully updated store <strong>${store.name}</strong>.
-    <a href="/stores/${store.slug}">View store</a>`
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store →</a>`
   );
-
   res.redirect(`/stores/${store._id}/edit`);
+  // Redriect them the store and tell them it worked
 };
